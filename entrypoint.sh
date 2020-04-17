b81334128740e7cc9e8ff4240ac96bb512a13993
Daniel Puscher <dev@daniel-puscher.de>
2020-04-17 12:26:58 +0200

    Use updated rubocop version from PR

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
+version() {
+  if [ -n "$1" ]; then
+    echo "-v $1"
+  fi
+}
+
+install_extensions() {
+  if [ -n "$1" ]; then
+    echo "$1" | sed "s/[^ ][^ ]*/'&'/g" | xargs gem install
+  else
+    gem install -N rubocop-rails rubocop-performance rubocop-rspec rubocop-i18n rubocop-rake
+  fi
+}
+
 cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+gem install rubocop $(version $INPUT_RUBOCOP_VERSION)
+
+install_extensions ${INPUT_RUBOCOP_EXTENSIONS}
 
 rubocop ${INPUT_RUBOCOP_FLAGS} \
   | reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
